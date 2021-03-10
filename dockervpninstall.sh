332cd57e9ed83740f3c5a4efd41234f20b4253d7
fiber-optics <30389098+fiber-optics@users.noreply.github.com>
2021-03-11 01:09:40 +0700

    Add files via upload

---
+++ dockervpninstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+apt update
+apt-get install  -y   apt-transport-https     ca-certificates     curl     gnupg-agent     software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+apt update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+docker pull hwdsl2/ipsec-vpn-server
+echo "VPN_IPSEC_PSK=tokyo
+VPN_USER=vpnuser
+VPN_PASSWORD=baNN0jWLqK" > vpn.env
+docker run --name ipsec-vpn-server --env-file ./vpn.env --restart=always -p 500:500/udp -p 4500:4500/udp -d --privileged hwdsl2/ipsec-vpn-server
+sleep 2
+docker logs ipsec-vpn-server
